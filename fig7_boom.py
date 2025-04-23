8a3f212231fde44ba57c90136dbd07809a62a440
Cole Strickler <colestrickler@gmail.com>
2025-04-23 15:26:38 -0500

    bold and larger font figures for paper

--- fig7_boom.py
+++ fig7_boom.py
@@ -34,21 +34,26 @@ for i in [1, 2, 4, 8, 16]: # column sizes
 df_filtered = df[df["DB Organization"] != "row"]
 
 # Step 4: Plot the normalized data
-plt.figure(figsize=(12, 6))
+plt.figure(figsize=(12, 6), dpi=300)
 sns.set_style("whitegrid")
+sns.set_context("paper", font_scale=1.6)  # Increase overall font scale
 
 # Create the grouped bar chart with normalized values
 sns.barplot(x="Column Size", y="Normalized Time(Cycles)", hue="DB Organization", data=df_filtered)
+# Make tick labels bigger
+plt.xticks(fontsize=18)
+plt.yticks(fontsize=18)
+
 
 # Add a horizontal black line at y=1.0 for row store normalization reference
 plt.axhline(y=1.0, color="black", linestyle="-", linewidth=2, label="Row")
 
 # Labels and title
 plt.ylim((0.0, 2.0))
-plt.xlabel("Column Size (bytes)")
-plt.ylabel("Normalized Exec. Time (Cycles)")
-plt.title("Fig 7 Boom")
-plt.legend(title="DB Organization")
+plt.xlabel("Column Size (bytes)", fontsize=18, fontweight="bold")
+plt.ylabel("Normalized Exec. Time (Cycles)", fontsize=18, fontweight="bold")
+#plt.title("Fig 7 Boom", fontsize=20, fontweight="bold")
+plt.legend(title="DB Organization", fontsize=14, title_fontsize=14)
 
 # Show the plot
 plt.savefig("image/fig7_boom.png")
